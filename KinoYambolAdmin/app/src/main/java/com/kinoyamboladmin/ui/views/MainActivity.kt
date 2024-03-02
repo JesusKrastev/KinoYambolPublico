ec1c5e38c4bec73672200d87cfaf9fff3d72c23e
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-02 13:45:22 +0100

    feat: Changed MainActivity

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/views/MainActivity.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/views/MainActivity.kt
@@ -3,25 +3,26 @@ package com.kinoyamboladmin.ui.views
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kinoyamboladmin.ui.features.seemovies.MoviesListViewModel
+import com.kinoyamboladmin.ui.navigation.MovieNavHost
 import com.kinoyamboladmin.ui.theme.KinoYambolAdminTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-	        KinoYambolAdminTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
+            KinoYambolAdminTheme(
+                darkTheme = true
+            ) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MovieNavHost()
                 }
             }
         }

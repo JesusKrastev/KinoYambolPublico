e0f091ceda6d54e4fcf6f4cc479d2ed43fd9607d
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-02 13:28:26 +0100

    feat: Added movie model

---
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/models/Movie.kt
@@ -0,0 +1,19 @@
+package com.kinoyamboladmin.models
+
+import androidx.compose.ui.graphics.ImageBitmap
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class Movie(
+    val id: Int,
+    val image: String,
+    val title: String,
+    val description: String,
+    val genders: List<String>,
+    val languages: List<String>,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val duration: Int,
+    val trailerLink: String,
+    val price: Double
+)

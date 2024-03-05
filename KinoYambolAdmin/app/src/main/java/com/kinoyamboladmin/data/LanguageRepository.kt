4250767fd858aeee1554897e7668f4af892313de
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-05 23:32:26 +0100

    feat: Added coroutines to repositories

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/LanguageRepository.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/LanguageRepository.kt
@@ -2,9 +2,13 @@ package com.kinoyamboladmin.data
 
 import com.kinoyamboladmin.data.mocks.language.LanguageDaoMock
 import com.kinoyamboladmin.models.Language
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class LanguageRepository(
     private val dao: LanguageDaoMock
 ) {
-    suspend fun get(): MutableList<Language> = dao.get().map { it.toLanguage() }.toMutableList()
+    suspend fun get(): MutableList<Language> = withContext(Dispatchers.IO) {
+        dao.get().map { it.toLanguage() }.toMutableList()
+    }
 }
--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/MovieRepository.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/MovieRepository.kt
@@ -3,14 +3,26 @@ package com.kinoyamboladmin.data
 import com.kinoyamboladmin.data.mocks.movie.MovieDaoMock
 import com.kinoyamboladmin.data.mocks.movie.MovieMock
 import com.kinoyamboladmin.models.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
     private val dao: MovieDaoMock
 ) {
-    suspend fun get(): MutableList<Movie> = dao.get().map { it.toMovie() }.toMutableList()
-    suspend fun get(id: Int): Movie? = dao.get(id)?.toMovie()
-    suspend fun insert(movie: Movie) = dao.insert(movie.toMovieMock())
-    suspend fun update(newMovie: Movie) = dao.update(newMovie.toMovieMock())
-    suspend fun delete(id: Int) = dao.delete(id)
+    suspend fun get(): MutableList<Movie> = withContext(Dispatchers.IO) {
+        dao.get().map { it.toMovie() }.toMutableList()
+    }
+    suspend fun get(id: Int): Movie? = withContext(Dispatchers.IO) {
+        dao.get(id)?.toMovie()
+    }
+    suspend fun insert(movie: Movie) = withContext(Dispatchers.IO) {
+        dao.insert(movie.toMovieMock())
+    }
+    suspend fun update(newMovie: Movie) = withContext(Dispatchers.IO) {
+        dao.update(newMovie.toMovieMock())
+    }
+    suspend fun delete(id: Int) = withContext(Dispatchers.IO) {
+        dao.delete(id)
+    }
 }

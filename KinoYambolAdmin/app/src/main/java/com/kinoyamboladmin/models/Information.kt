d01cb43db00d0572e2be4e9dab25ff78354c075a
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-05 23:08:50 +0100

    feat: Added movie information dao an repository

---
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/models/Information.kt
@@ -0,0 +1,12 @@
+package com.kinoyamboladmin.models
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class Information(
+    val id: Int,
+    val movieId: Int,
+    val language: Int,
+    val date: LocalDate,
+    val time: LocalTime
+)

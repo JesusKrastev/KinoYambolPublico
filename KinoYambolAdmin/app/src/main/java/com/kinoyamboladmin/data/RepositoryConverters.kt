4d2c8b973983d00a2707cb4309a95e682a3b2b4f
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-05 19:04:10 +0100

    feat: Fixed some issues with deleted Language and Theme

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/RepositoryConverters.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/RepositoryConverters.kt
@@ -1,12 +1,7 @@
 package com.kinoyamboladmin.data
 
-import com.kinoyamboladmin.data.mocks.language.LanguageMock
 import com.kinoyamboladmin.data.mocks.movie.MovieMock
-import com.kinoyamboladmin.data.mocks.theme.ThemeMock
-import com.kinoyamboladmin.models.Language
 import com.kinoyamboladmin.models.Movie
-import com.kinoyamboladmin.models.Settings
-import com.kinoyamboladmin.models.Theme
 
 fun MovieMock.toMovie(): Movie =
     Movie(
@@ -36,28 +31,4 @@ fun Movie.toMovieMock(): MovieMock =
         duration = duration,
         trailerLink = trailerLink,
         price = price
-    )
-
-fun LanguageMock.toLanguage(): Language =
-    Language(
-        id = id,
-        language = language
-    )
-
-fun Language.toLanguageMock(): LanguageMock =
-    LanguageMock(
-        id = id,
-        language = language
-    )
-
-fun Theme.toThemeMock(): ThemeMock =
-    ThemeMock(
-        id = id,
-        theme = theme
-    )
-
-fun ThemeMock.toTheme(): Theme =
-    Theme(
-        id = id,
-        theme = theme
     )
--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/features/settings/SettingsUiState.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/features/settings/SettingsUiState.kt
@@ -1,8 +1,6 @@
 package com.kinoyamboladmin.ui.features.settings
 
-import com.kinoyamboladmin.models.Language
-
 data class SettingsUiState(
-    val language: String = "Español",
-    val theme: String = "Oscuro"
+    val language: String = "Sistema",
+    val theme: String = "Sistema"
 )

5fe16740839509f354492c308aeba1e9f544c871
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-06 23:42:46 +0100

    feat: Schedule and ScheduleRepository updated

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/ScheduleRepository.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/ScheduleRepository.kt
@@ -1,24 +1,41 @@
 package com.kinoyamboladmin.data
 
+import com.kinoyamboladmin.data.mocks.language.LanguageDaoMock
 import com.kinoyamboladmin.data.mocks.schedule.ScheduleDaoMock
+import com.kinoyamboladmin.data.mocks.schedule.ScheduleMock
 import com.kinoyamboladmin.models.Schedule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ScheduleRepository @Inject constructor(
-    private val dao: ScheduleDaoMock
+    private val scheduleDao: ScheduleDaoMock,
+    private val languageDao: LanguageDaoMock
 ) {
     suspend fun get(): MutableList<Schedule> = withContext(Dispatchers.IO) {
-        dao.get().map { it.toSchedule() }.toMutableList()
+        scheduleDao.get().map { it.toSchedule() }.toMutableList()
     }
     suspend fun get(id: Int): Schedule? = withContext(Dispatchers.IO) {
-        dao.get(id)?.toSchedule()
+        scheduleDao.get(id)?.toSchedule()
     }
     suspend fun insert(schedule: Schedule) = withContext(Dispatchers.IO) {
-        dao.insert(schedule.toScheduleMock())
+        scheduleDao.insert(schedule.toScheduleMock())
     }
     suspend fun update(newSchedule: Schedule) = withContext(Dispatchers.IO) {
-        dao.update(newSchedule.toScheduleMock())
+        scheduleDao.update(newSchedule.toScheduleMock())
     }
+
+    fun Schedule.toScheduleMock(): ScheduleMock =
+        ScheduleMock(
+            id = id,
+            language = language.id,
+            movieSchedule = movieSchedule
+        )
+
+    fun ScheduleMock.toSchedule(): Schedule =
+        Schedule(
+            id = id,
+            language = languageDao.get(language)!!.toLanguage(),
+            movieSchedule = movieSchedule
+        )
 }

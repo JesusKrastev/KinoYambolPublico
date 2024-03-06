56bfa71896691b2a8cfe60c9105fb93e83c42f65
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-06 15:34:26 +0100

    feat: Added a field call movieSchedule to Information and InformationMock

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/mocks/information/InformationMock.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/mocks/information/InformationMock.kt
@@ -7,6 +7,5 @@ data class InformationMock(
     val id: Int,
     val movieId: Int,
     val language: Int,
-    val date: LocalDate,
-    val hours: List<LocalTime>
+    val movieSchedule: HashMap<LocalDate, List<LocalTime>>
 )

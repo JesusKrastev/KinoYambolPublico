fb87274b402e895fa11fdcbfe50c7aea2d4d2abc
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-06 22:56:15 +0100

    feat: Modified Movie and MovieRepository

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/MovieRepository.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/data/MovieRepository.kt
@@ -2,24 +2,52 @@ package com.kinoyamboladmin.data
 
 import com.kinoyamboladmin.data.mocks.movie.MovieDaoMock
 import com.kinoyamboladmin.data.mocks.movie.MovieMock
+import com.kinoyamboladmin.data.mocks.schedule.ScheduleDaoMock
 import com.kinoyamboladmin.models.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
-    private val dao: MovieDaoMock
+    private val movieDao: MovieDaoMock,
+    private val scheduleDaoMock: ScheduleDaoMock
 ) {
     suspend fun get(): MutableList<Movie> = withContext(Dispatchers.IO) {
-        dao.get().map { it.toMovie() }.toMutableList()
+        movieDao.get().map { it.toMovie() }.toMutableList()
     }
     suspend fun get(id: Int): Movie? = withContext(Dispatchers.IO) {
-        dao.get(id)?.toMovie()
+        movieDao.get(id)?.toMovie()
     }
     suspend fun insert(movie: Movie) = withContext(Dispatchers.IO) {
-        dao.insert(movie.toMovieMock())
+        movieDao.insert(movie.toMovieMock())
     }
     suspend fun update(newMovie: Movie) = withContext(Dispatchers.IO) {
-        dao.update(newMovie.toMovieMock())
+        movieDao.update(newMovie.toMovieMock())
     }
+
+    fun MovieMock.toMovie() : Movie =
+        Movie(
+            id = id,
+            image = image,
+            name = name,
+            description = description,
+            genders = genders,
+            schedules = schedules.map { scheduleDaoMock.get(it)!!.toSchedule() },
+            duration = duration,
+            trailerLink = trailerLink,
+            price = price
+        )
+
+    fun Movie.toMovieMock() : MovieMock =
+        MovieMock(
+            id = id,
+            image = image,
+            name = name,
+            description = description,
+            genders = genders,
+            schedules = schedules.map { it.id },
+            duration = duration,
+            trailerLink = trailerLink,
+            price = price
+        )
 }

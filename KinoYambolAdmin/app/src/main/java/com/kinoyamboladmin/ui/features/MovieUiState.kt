c226f03ea3f4f2ff1c3f6cc983a43183c9ca4381
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-06 21:10:59 +0100

    feat: Modified MovieUiState

--- KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/features/MovieUiState.kt
+++ KinoYambolAdmin/app/src/main/java/com/kinoyamboladmin/ui/features/MovieUiState.kt
@@ -1,8 +1,5 @@
 package com.kinoyamboladmin.ui.features
 
-import com.kinoyamboladmin.models.Movie
-import com.kinoyamboladmin.models.Schedule
-
 data class MovieUiState(
     val id: Int = 0,
     val image: String = "",
@@ -14,29 +11,3 @@ data class MovieUiState(
     val trailerLink: String = "",
     val price: Double = 0.0
 )
-
-fun Movie.toMovieUiState(): MovieUiState =
-    MovieUiState(
-        id = id,
-        image = image,
-        name = name,
-        description = description,
-        genders = genders,
-        schedules = schedules,
-        duration = duration,
-        trailerLink = trailerLink,
-        price = price
-    )
-
-fun MovieUiState.toMovie(): Movie =
-    Movie(
-        id = id,
-        image = image,
-        name = name,
-        description = description,
-        genders = genders,
-        schedules = schedules,
-        duration = duration,
-        trailerLink = trailerLink,
-        price = price
-    )
